6cd23a9541b30b5baf5b8e0b3eb160e501b12a20
Dylan Dumesnil <dadumesnil@gmail.com>
2023-08-07 17:59:25 -0700

    Log warning when getting a cached view model that has settings that differ from the settings being passed in

--- Source/MDViewModel/Private/Interfaces/MDViewModelCacheInterface.cpp
+++ Source/MDViewModel/Private/Interfaces/MDViewModelCacheInterface.cpp
@@ -1,6 +1,7 @@
 ﻿#include "Interfaces/MDViewModelCacheInterface.h"
 
 #include "Util/MDViewModelInstanceKey.h"
+#include "Util/MDViewModelLog.h"
 #include "ViewModel/MDViewModelBase.h"
 
 uint64 IMDViewModelCacheInterface::LastHandle = 0;
@@ -13,7 +14,7 @@ UMDViewModelBase* IMDViewModelCacheInterface::GetOrCreateViewModel(const UObject
 	}
 
 	const FMDViewModelInstanceKey Key = { CachedViewModelKey, ViewModelClass };
-	if (!ensure(Key.IsValid()))
+	if (!Key.IsValid())
 	{
 		return nullptr;
 	}
@@ -24,6 +25,17 @@ UMDViewModelBase* IMDViewModelCacheInterface::GetOrCreateViewModel(const UObject
 		ViewModel = NewObject<UMDViewModelBase>(GetTransientPackage(), Key.ViewModelClass);
 		ViewModel->InitializeViewModelWithContext(ViewModelSettings, GetViewModelOwner(), WorldContextObject);
 	}
+#if !(UE_BUILD_SHIPPING || UE_BUILD_TEST)
+	else if (ViewModel->GetViewModelSettings() != ViewModelSettings)
+	{
+		// TODO - The view model editor should be able to facilitate "Make my settings the same as that other view model assignment" to make this less likely to happen
+		FString CachedSettingsString;
+		ViewModel->GetViewModelSettings().ExportTextItem(CachedSettingsString, {}, nullptr, PPF_None, nullptr);
+		FString RequestedSettingsString;
+		ViewModelSettings.ExportTextItem(RequestedSettingsString, {}, nullptr, PPF_None, nullptr);
+		UE_LOG(LogMDViewModel, Warning, TEXT("The cached view model [%s] has different view model settings than currently being requested:\r\nCached: %s\r\nRequested: %s"), *ViewModel->GetName(), *CachedSettingsString, *RequestedSettingsString);
+	}
+#endif
 
 	return ViewModel;
 }
