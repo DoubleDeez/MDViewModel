14c143bfdddb3935c71c4b6cc7566504ff8e110c
Dylan Dumesnil <dadumesnil@gmail.com>
2024-04-30 14:35:12 -0700

    Fix manually assigned view models broadcasting a null VM on initialization even though the VM had been previously set

--- Source/MDViewModel/Private/ViewModelProviders/MDViewModelProvider_Manual.cpp
+++ Source/MDViewModel/Private/ViewModelProviders/MDViewModelProvider_Manual.cpp
@@ -1,10 +1,11 @@
 #include "ViewModelProviders/MDViewModelProvider_Manual.h"
 
+#include "Interfaces/MDViewModelRuntimeInterface.h"
 
 UE_DEFINE_GAMEPLAY_TAG(TAG_MDVMProvider_Manual, "MDVM.Provider.Manual");
 
 UMDViewModelBase* UMDViewModelProvider_Manual::SetViewModel(IMDViewModelRuntimeInterface& Object, const FMDViewModelAssignment& Assignment, const FMDViewModelAssignmentData& Data)
 {
-	// Intentional no-op, external systems are expected to do the assignment
-	return nullptr;
+	// Get the existing view model if there is one, since it would have been manually set
+	return Object.GetViewModel(FMDViewModelAssignmentReference(Assignment));
 }
